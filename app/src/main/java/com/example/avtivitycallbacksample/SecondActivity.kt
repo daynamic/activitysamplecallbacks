3cba1c091cae686d0fc6a744866ae89363ac8a87
Akshat Sharma <Akshat.Sharma@SRKayCG.com>
2019-06-13 12:02:09 +0530

    First commit for implementation for CI/CD

---
+++ app/src/main/java/com/example/avtivitycallbacksample/SecondActivity.kt
@@ -0,0 +1,43 @@
+package com.example.avtivitycallbacksample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class SecondActivity : AppCompatActivity() {
+    val msg="SecondActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        Log.d(msg,"In on create function")
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d(msg,"In on start function")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(msg,"In on resume function")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(msg,"In on pause function")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(msg,"In on stop function")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(msg,"In on restart function")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(msg,"In on destroy function")
+    }
+}
